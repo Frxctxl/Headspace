f22b4ae2ce6e84e0f1d42afff11c4966ee9716f4
Jay <jayden_benston@yahoo.com>
2024-08-19 17:51:29 -0400

    finished user routes

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -7,6 +7,14 @@ module.exports = {
     res.json(users)
   },
 
+  async getUser(req, res) {
+    const user = await User.findById(req.body.userId);
+
+    res.json({
+      user: user
+    })
+  },
+
   async createUser(req, res) {
     const newUser = await User.create({
       username: req.body.username,
@@ -17,5 +25,29 @@ module.exports = {
       message: 'Done',
       user: newUser
     });
-  }
+  },
+
+  async updateUser(req, res) {
+    const updatedUser = await User.updateOne({
+      _id: req.body.userId
+    }, {
+      username: req.body.username,
+      email: req.body.email
+    })
+
+    res.json({
+      message: 'Done',
+      user: updatedUser
+    })
+  },
+
+  async deleteUser(req, res) {
+    await User.deleteOne({
+      _id: req.body.userId
+    });
+
+    res.json({
+      message: 'Done'
+    })
+  },
 }
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 const user_controller = require('../controllers/user_controller');
 
 router.get('/users', user_controller.getAllUsers);
-router.post('/users', user_controller.createUser);
+router.get('/user', user_controller.getUser);
+router.post('/user', user_controller.createUser);
+router.put('/user', user_controller.updateUser);
+router.delete('/user', user_controller.deleteUser);
 
 module.exports = router;
